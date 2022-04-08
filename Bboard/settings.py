3b175deaa4be1d9cb947a116b8cdf3aff3f54f59
Bulat <expowheella@gmail.com>
2022-04-08 10:10:39 +0300

    Bulletin model is adjusted, Category model is created

--- Bboard/settings.py
+++ Bboard/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# The location to saving uploaded files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Any files were uploaded will be show up under this path
+MEDIA_URL = "/media/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local
+    'myboardapp',
 ]
 
 MIDDLEWARE = [
--- myboardapp/models.py
+++ myboardapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 # User model
@@ -11,12 +12,49 @@ class User(models.Model):
     def __str__(self):
         return f'{self.user.name.title()}'
 
+
+# here is a function which will be used as a callable
+# it will be called to obtain the upload path
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+# Bulletins category model
+class CategoryModel(models.Model):
+    # this field is for creating bulletin category in the admin panel
+    category_name = models.CharField(max_length=255,
+                                     unique=True,
+                                     )
+
+    # we use this function to return the bulletin category of an instance
+    def __str__(self):
+        return f'{self.category_name}'
+
+
 # Bulletin model
 class Bulletin(models.Model):
     # bulletin has its author
     # if an author is deleted, his bulletins will be deleted too
+    # this is One to Many field -> built-in User has its examples which are authors
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # bulletin has content
     content = models.TextField()
 
+    # it is possible to adjust a bulletin with a file
+    # file will be saved to -> watch settings.py -> MEDIA_ROOT
+    file = models.FileField(models.FileField(upload_to=user_directory_path,
+                                             blank=True,
+                                             # null=True,
+                                             verbose_name='File Upload'))
+
+    def __str__(self):
+        return self.file.verbose_name
+
+    # each bulletin related to some category
+    # that is why we use Many-To-One field
+    bulletin_category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+
+    # add bulletin created date
+    date_created = models.DateTimeField(default=timezone.now)
